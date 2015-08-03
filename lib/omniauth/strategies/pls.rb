d4da6ba24ea9ffac8fa16e079c5b0912a4e7f7c3
draffensperger <d.raffensperger@gmail.com>
2015-08-03 10:18:17 -0400

    Fix PLS url and tidy up

--- lib/omniauth/strategies/pls.rb
+++ lib/omniauth/strategies/pls.rb
@@ -7,26 +7,23 @@ module OmniAuth
       option :name, 'pls'
 
       option :client_options, {
-        site: 'https://pls.herokuapp.com',
-        authorize_url: '/oauth/mpdx',
+        site: 'https://www.myletterservice.org',
+        authorize_url: '/oauth/authorize',
         token_url: '/oauth/token'
       }
 
       uid { raw_info['id'] }
 
       info do
-        {
-        }
+        {}
       end
 
       extra do
-        {
-          'raw_info' => raw_info
-        }
+        { 'raw_info' => raw_info }
       end
 
       def raw_info
-        @raw_info ||= {} #MultiJson.decode(access_token.get('/v2/people/@me/@self?format=json').body)['entry']
+        @raw_info ||= {}
       end
 
       def request_phase
